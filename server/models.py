d63882c64dc008b54eeb79e3a5b1c4a4f41715ca
Your Name <jymohcooler@gmail.com>
2023-09-23 02:57:17 +0300

    Complete lab

--- server/models.py
+++ server/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Enum
 from sqlalchemy.orm import validates
 db = SQLAlchemy()
 
@@ -8,12 +9,26 @@ class Author(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String, unique=True, nullable=False)
-    phone_number = db.Column(db.String)
+    phone_number = db.Column(db.String(10))
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     def __repr__(self):
-        return f'Author(id={self.id}, name={self.name})'
+        return f'Authorid={self.id}, name={self.name})'
+    
+    @validates('name')
+    def validate_name(self,key,value):
+        if not value:
+            raise ValueError("not possible")
+        return value 
+    
+    
+    @validates('phone_number')
+    def validate_phone_number(self,key,value):
+        if len(value) != 10:
+            raise ValueError("not possible")
+        return value
+
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -22,7 +37,8 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     content = db.Column(db.String)
-    category = db.Column(db.String)
+    
+    category = db.Column(nullable=False)
     summary = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
@@ -30,3 +46,25 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+    @validates('title')
+    def validate_title(self,key,value):
+        if value not in ["Won't Believe","Secret","Top [number]","Guess"]:
+            raise ValueError("not possible")
+        return value
+    @validates('content')
+    def validate_content(self,key,value):
+        if len(value) < 250:
+            raise ValueError("not possible")
+        return value
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError("Post summary cannot exceed 250 characters.")
+        return summary
+    @validates('category')
+    def validate_category(self,key,category):
+        if category not in ["Fiction", "Non-Fiction"]:
+            raise ValueError("not possible")
+        return category
+
+    
